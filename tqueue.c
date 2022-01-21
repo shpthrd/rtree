09ca115eab586f97a42056c1a3d6f07c84168955
nanomugen <eltonlagr@gmail.com>
2022-01-21 14:42:14 -0300

    counting time

--- tqueue.c
+++ tqueue.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
 
 #define NUM_THREADS 4
 #define M 6
-#define K 50
+#define K 10000000 //limit
+
 typedef struct node{
-    int x1,y1,x2,y2;
+    long x1,y1,x2,y2;
     struct node* child[M+1]; 
 } node;
 typedef struct item{
@@ -19,22 +21,32 @@ typedef struct queue{
     struct item* last;
 } queue;
 
-int counting_node = 0;
-int counting_pop = 0;
-int counting_push = 0;
+
+long counting_node = 0;
+long counting_free_node = 0;
+long counting_pop = 0;
+long counting_push = 0;
 queue* q;
 node s_node;
 pthread_mutex_t mut_f = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mut_l = PTHREAD_MUTEX_INITIALIZER;
+clock_t start, end;
+double cpu_time_used;
 
-node* createNode(int x1,int y1, int x2,int y2){
+
+//=====================================================================================================
+//NODE FUNCTIONS
+//=====================================================================================================
+
+node* createNode(long x1,long y1, long x2,long y2){
     node* n = (node*)malloc(sizeof(node));
+    counting_node++;
     n->x1 = x1;
     n->y1 = y1;
     n->x2 = x2;
     n->y2 = y2;
     if(x2-x1 > 1){
-        int i;
+        long i;
         for(i=0;i<M;i++){
             n->child[i] = createNode(x1 + i*(x2 - x1)/M,x1 + i*(x2 - x1)/M,x1 + (i+1)*(x2 - x1)/M,x1 + (i+1)*(x2 - x1)/M);
         }
@@ -43,9 +55,9 @@ node* createNode(int x1,int y1, int x2,int y2){
 }
 
 void printTree(node* n){
-	int i;
-    counting_node++;
-	printf("(%d,%d)(%d,%d)\n",n->x1,n->y1,n->x2,n->y2);
+	long i;
+    //counting_node++;
+	printf("(%ld,%ld)(%ld,%ld)\n",n->x1,n->y1,n->x2,n->y2);
 	i=0;
 	while(n->child[i] != NULL && i<=M){
 			printTree(n->child[i]);
@@ -53,9 +65,9 @@ void printTree(node* n){
 	}
 }
 
-int Overlap(node* r1, node* r2){
+long Overlap(node* r1, node* r2){
     if(r1 == NULL || r2 == NULL) return 0;
-	int overX =0, overY = 0;
+	long overX =0, overY = 0;
 	if((r1->x1 <= r2->x1 && r1->x2 >= r2->x1) || (r2->x1 <= r1->x1 && r2->x2 >= r1->x1)){
 		overX = 1;
 	}
@@ -65,6 +77,23 @@ int Overlap(node* r1, node* r2){
 	if(overY ==1 && overX == 1) return 1; else return 0;
     
 }
+
+void freeNode(node** node){
+	if(node == NULL) return;
+	long i = 0;
+	for(i = 0; i < M+1; i++){
+		if((*node)->child[i] != NULL)
+			freeNode(&(*node)->child[i]);
+	}
+	free(*node);
+	*node = NULL;
+    counting_free_node++;
+}
+
+
+//=====================================================================================================
+//QUEUE FUNCTIONS
+//=====================================================================================================
 
 node* pop(queue* q){
     if(q == NULL) return NULL;
@@ -103,7 +132,9 @@ void push(queue* q,node* n){
  
 
 
-
+//=====================================================================================================
+//PTHREAD FUNCTIONS
+//=====================================================================================================
 void *search(void* param)
 {
     while(1){
@@ -111,6 +142,9 @@ void *search(void* param)
         pthread_mutex_lock(&mut_l);
         if(q->init == NULL){
             if(counting_pop == counting_node){
+                end = clock();
+	            cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	            printf("time elapsed: %f\n",cpu_time_used);
                 pthread_mutex_unlock(&mut_f);
                 pthread_mutex_unlock(&mut_l);
                 break;
@@ -125,13 +159,14 @@ void *search(void* param)
                 node* n = q->init->n;
                 q->init = it->next;
                 free(it);
-                printf("%ld: pop(%d,%d)(%d,%d) - counting: %d\n",pthread_self(),n->x1,n->y1,n->x2,n->y2,++counting_pop);
+                counting_pop++;
+                //printf("%ld: pop(%ld,%ld)(%ld,%ld) - counting: %ld\n",pthread_self(),n->x1,n->y1,n->x2,n->y2,counting_pop);
                 pthread_mutex_unlock(&mut_f);
                 if(Overlap(n,&s_node) == 1){
-                    printf("here if\n");
+                    //printf("here if\n");
                     pthread_mutex_lock(&mut_f);
                     pthread_mutex_lock(&mut_l);
-                    int i = 0;
+                    long i = 0;
                     while(n->child[i] != NULL && i<M){
                         item* it2 = (item*) malloc(sizeof(item));
                         it2->next = NULL;
@@ -144,7 +179,8 @@ void *search(void* param)
                             q->last->next = it2;
                             q->last = it2;
                         }
-                        printf("%ld: push(%d,%d)(%d,%d) - counting %d\n",pthread_self(),it2->n->x1,it2->n->y1,it2->n->x2,it2->n->y2,++counting_push);
+                        counting_push++;
+                        //printf("%ld: push(%ld,%ld)(%ld,%ld) - counting %ld\n",pthread_self(),it2->n->x1,it2->n->y1,it2->n->x2,it2->n->y2,counting_push);
                         i++;
                     }
                     
@@ -160,12 +196,13 @@ void *search(void* param)
                 free(it);
                 pthread_mutex_unlock(&mut_f);
                 pthread_mutex_unlock(&mut_l);
-                printf("%ld: pop(%d,%d)(%d,%d) - counting: %d\n",pthread_self(),n->x1,n->y1,n->x2,n->y2,++counting_pop);
+                counting_pop++;
+                //printf("%ld: pop(%ld,%ld)(%ld,%ld) - counting: %ld\n",pthread_self(),n->x1,n->y1,n->x2,n->y2,counting_pop);
                 if(Overlap(n,&s_node) == 1){
-                    printf("here else\n");
+                    //printf("here else\n");
                     pthread_mutex_lock(&mut_f);
                     pthread_mutex_lock(&mut_l);
-                    int i = 0;
+                    long i = 0;
                     while(n->child[i] != NULL && i<M){
                         item* it2 = (item*) malloc(sizeof(item));
                         it2->next = NULL;
@@ -178,7 +215,8 @@ void *search(void* param)
                             q->last->next = it2;
                             q->last = it2;
                         }
-                        printf("%ld: push(%d,%d)(%d,%d) - counting %d\n",pthread_self(),it2->n->x1,it2->n->y1,it2->n->x2,it2->n->y2,++counting_push);
+                        counting_push++;
+                        //printf("%ld: push(%ld,%ld)(%ld,%ld) - counting %ld\n",pthread_self(),it2->n->x1,it2->n->y1,it2->n->x2,it2->n->y2,counting_push);
                         i++;
                     }
                     
@@ -193,24 +231,32 @@ void *search(void* param)
 
 int main(){
     q = (queue*)malloc(sizeof(queue));
+    start = clock();
     node* root = createNode(0,0,K,K);
+    end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("time elapsed create: %f\n",cpu_time_used);
     s_node.x1=0;
     s_node.y1=0;
     s_node.x2=K;
     s_node.y2=K;
-    
-    printTree(root);
-    printf("counting node: %d\n",counting_node);
+    printf("counting node: %ld\n",counting_node);
+    /* //printTree(root);
+    printf("counting node: %ld\n",counting_node);
+    start = clock();
     push(q,root);
 
     pthread_t threads[NUM_THREADS];
-    int i;
+    long i;
     for (i = 0; i < NUM_THREADS; ++i) {
         pthread_create(&threads[i], NULL, search, NULL);
     }
 
     for (i = 0; i < NUM_THREADS; ++i) {
         pthread_join(threads[i], NULL);
-    }
+    } */
+    free(q);
+    freeNode(&root);
+    printf("counting free node: %ld\n",counting_free_node);
     exit(EXIT_SUCCESS);
 }
